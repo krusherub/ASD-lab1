cd6dcc0a1e02a853411179914f6ba5345ba28643
V1tali1ty <vitalikkryvonosiuk@gmail.com>
2021-02-15 12:25:40 +0200

    Added NotArrayList class

--- algorithms lab1/Program.cs
+++ algorithms lab1/Program.cs
@@ -10,13 +10,28 @@ namespace algorithms_lab1
         //вернуть прайват
         static void Main(string[] args)
         {
-            LinkedListOneWay<int> s = new LinkedListOneWay<int>();
-            s.addFirst(1);
-            
-            s.addLast(2);
-            s.addLast(3);
-            s.removeLast();
-            
+            //LinkedListOneWay<int> s = new LinkedListOneWay<int>();
+            //s.addFirst(1);
+
+            //s.addLast(2);
+            //s.addLast(3);
+            //s.removeLast();
+
+            NotArrayList array = new NotArrayList();
+            for(int i=1; i<15; i++)
+            {
+                array.addToTheEnd(i);
+            }
+            for (int i = 0; i < 14; i++)
+            {
+                Console.WriteLine(array.getElement(i));
+            }
+            Console.WriteLine("----------------------------");
+            array.deleteAtTheBeginning();
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(array.getElement(i));
+            }
         }
     }
 
